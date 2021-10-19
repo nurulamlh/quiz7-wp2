3f9d6f3a2bd9256f890bc16fc36c085679564708
nurulamlh <90683203+nurulamlh@users.noreply.github.com>
2021-10-19 22:12:43 +0700

    Add files via upload
    tugas quiz pra uts, Nurul Amaliah bu

---
+++ Tugas7_Model.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Tugas7_Model extends CI_Model
+{
+	public $harga;
+
+	public function proses($data)
+	{
+		if ($data == "Nike"){
+			$this ->harga = 375000;
+			} elseif($data=="Adidas"){
+				$this ->harga=300000;
+			} elseif($data=="Kickers"){
+				$this ->harga=250000;
+			} elseif($data=="Eiger"){
+				$this->harga=275000;
+			} else {
+				$this ->harga=400000;
+			}
+
+		return $this->harga;
+
+
+		}
+	}
